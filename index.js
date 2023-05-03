0630d82266cd179677d46fb7516e36c27b5514e7
manjurul17002 <manjurul17002@gmail.com>
2023-05-03 23:02:33 +0600

    json added

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require ('express')
 const app = express();
+const cors = require ('cors')
 const port = process.env.PORT || 5000;
+
+const chef = require('./data/chef.json');
+
+app.use(cors());
 
 app.get ('/', (req,res) =>{
     res.send('assignmet-10')
 })
 
+app.get('/chef', (req, res) =>{
+    res.send(chef)
+})
 
 app.listen(port, () =>{
     console.log(`assignment API is Running on port : ${port}`);
